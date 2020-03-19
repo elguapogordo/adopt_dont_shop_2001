ecee189088da516cf598635953dc7e0d643f3de6
Jesse Gietzen <gietzenjesse@gmail.com>
2020-03-18 20:41:26 -0600

    add shelter details page, test

---
+++ spec/features/shelters/user_can_see_shelter_details_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.describe "as a visitor", type: :feature do
+  it "can visit the shelter show page and see details" do
+    shelter1 = Shelter.create(name: "Buddy Center",
+                              address: "4556 Castleton Court",
+                              city: "Castle Rock",
+                              state: "CO",
+                              zip: "80109")
+    visit "/shelters/#{shelter1.id}"
+
+    expect(page).to have_content(shelter1.name)
+    expect(page).to have_content(shelter1.address)
+    expect(page).to have_content(shelter1.city)
+    expect(page).to have_content(shelter1.state)
+    expect(page).to have_content(shelter1.zip)
+  end
+end
