2a56044e232eb5019cd57648b1df8ad3a576c27a
Jesse Gietzen <gietzenjesse@gmail.com>
2020-03-20 16:00:24 -0600

    add edit, update shelters

--- app/controllers/shelters_controller.rb
+++ app/controllers/shelters_controller.rb
@@ -18,6 +18,16 @@ class SheltersController < ApplicationController
     redirect_to '/shelters'
   end
 
+  def edit
+    @shelter = Shelter.find(params["id"])
+  end
+
+  def update
+    shelter = Shelter.find(params["id"])
+    shelter.update(shelter_params)
+    redirect_to "/shelters/#{params[:id]}"
+  end
+
   private
   def shelter_params
     params.permit(:name, :address, :city, :state, :zip)
