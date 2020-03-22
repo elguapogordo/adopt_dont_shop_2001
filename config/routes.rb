0a00ef7bfe38e5f8555ddaa407656a21f4e01b14
Jesse Gietzen <gietzenjesse@gmail.com>
2020-03-22 17:56:50 -0600

    add delete shelter from index

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   get '/shelters/:id/edit', to: 'shelters#edit'
   patch '/shelters/:id', to: 'shelters#update'
   delete '/shelters/:id', to: 'shelters#destroy'
+  get '/shelters/:id/delete', to: 'shelters#destroy'
 
   get '/shelters/:id/pets', to: 'shelter_pets#index'
   get '/shelters/:id/pets/new', to: 'shelter_pets#new'
